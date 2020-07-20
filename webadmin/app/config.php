f9fdcec33243a50c405076402223f98e0058fbcb
latifbaraba <latifbaraba88@gmail.com>
2020-07-20 19:21:58 +0700

    hapus gitignore

--- webadmin/app/config.php
+++ webadmin/app/config.php
@@ -2,5 +2,5 @@
 
 const WEB_URL = 'http://localhost:8080/happyco3/webadmin/';
 const WEB_URL_PUBLIC = 'http://localhost:8080/happyco3/webpublic/';
-// const WEB_URL = 'http://localhost:74/rumah-lelang/webadmin/';
+// const WEB_URL = 'http://localhost:74/rumah-lelang/webadmin/'; 
 // const WEB_URL_PUBLIC = 'http://localhost:74/rumah-lelang/webpublic/';
